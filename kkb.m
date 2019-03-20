ca46f380414323c84ed4b5ee739618481e4147c7
axytho <jojojo.jonas888@hotmail.com>
2019-03-20 11:56:47 +0100

    testDriven development!

---
+++ kkb.m
@@ -0,0 +1,6 @@
+function kkb(x, y, F, m, n)
+% kkb: calculates C = B^(+) * F * (A^(+))'
+% PREAMBLE: ^(+) is simply written as \ in MATLAB (psuedo inverse)
+% INPUT: x vector with dim m and y vector with dim n
+% VALUES INSIDE: 
+end
---
+++ testCases/kkbTest.m
@@ -0,0 +1,31 @@
+classdef kkbTest < matlab.unit.TestCase
+    %UNTITLED2 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        functionBasic
+    end
+    
+    methods(TestMethodSetup)
+        function loadParams(testCase)
+            testCase.functionBasic = @(x,y) x+y;
+        end
+    end
+    
+    methods(Test)
+        function testBasic(testCase)
+           % f = x + y
+           x = [0, 1];
+           y = [0, 1];
+           F = testCase.functionBasic(x, y');
+           m = size(x, 2);
+           n = size(y, 2);
+           % Our function is very simple, and the best result would be
+           % C = [0, 1; 1, 0], because we want 0*1*1+1*x*1+1*1*y+0*x*y
+           C = [0, 1; 1, 0];
+           CFunction = kkb(x, y, F, m, n);
+           testCase.verifyEqual(C, CFunction);
+        end
+    end
+end
+
